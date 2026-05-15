bba75c9287e2ef359ada221a94c477f50f8eb8b7
3.80GHz <shinra.dx@outlook.com>
2026-05-15 11:18:59 +0800

    添加应用配置文件

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ *  平行摩尔 Electron 微端 -- 应用配置文件
+ * 
+ *  @author C.R.H <shinra.dx@outlook.com>
+ *  @license MIT
+ */
+
+
+const WINDOW = require('./config/window.json');
+const URL = require('./config/url.json');
+const STORE_PATH = './data/client_data.json';
+
+
+module.exports = { WINDOW, URL, STORE_PATH };
