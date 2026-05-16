f4c8e7016686744d68184334251c5e136eeb8863
3.80GHz <shinra.dx@outlook.com>
2026-05-16 23:09:40 +0800

    完善electron-forge编译配置

--- forge.config.js
+++ forge.config.js
@@ -1,5 +1,35 @@
 module.exports = {
     packagerConfig: {
-        icon: './resources/favicon'
+        name: 'MoleVerseClient',
+        productName: '平行摩尔微端',
+        icon: './resources/favicon',
+        overwrite: true,
+        asar: false,
+        version: "0.0.1",
+        ignore: [
+            '.git',
+            '.vscode',
+            'node_modules/.cache',
+            '.npmrc',
+            'resources',
+            'forge.config.js',
+            '.gitignore',
+            'data/',
+        ],
     },
+    makers: [{
+        name: "@electron-forge/maker-squirrel",
+        config: {
+            "name": "electron_quick_start",
+        },
+    },{
+        name: "@electron-forge/maker-zip",
+        platforms: ["darwin"],
+    },{
+        name: "@electron-forge/maker-deb",
+        config: {},
+    },{
+        name: "@electron-forge/maker-rpm",
+        config: {},
+    }],
 };
